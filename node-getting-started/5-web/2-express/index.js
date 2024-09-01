450f9a53d0f968efc8a7d4f95061227aaf0e9631
Parsa <mihandoost.parsa@gmail.com>
2024-09-01 19:03:01 +0200

    organize folders

--- node-getting-started/5-web/2-express/index.js
+++ node-getting-started/5-web/2-express/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const server = express();
 
+server.get("/", (req, res) => {
+    res.send("Hello express!")
+})
+
 server.listen(4242, () => {
   console.log('Express Server is running...');
 });
